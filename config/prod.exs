70530ccbf2a014bf09ee421cf646fa5e22292cd4
Daryl Richter <daryl@revzilla.com>
2015-10-06 17:47:16 -0400

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :gebetsgruppe, Gebetsgruppe.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "gebetsgruppe.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :gebetsgruppe, Gebetsgruppe.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # ## SSL Support
 #
@@ -55,7 +63,3 @@ config :logger, level: :info
 #
 #     config :gebetsgruppe, Gebetsgruppe.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"

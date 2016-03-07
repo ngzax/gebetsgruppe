730ba3f458b7e882fa9ac4a2c06f7d2c00481726
Daryl Richter <daryl@revzilla.com>
2016-03-07 15:27:07 -0500

    Re-activate the integration tests, but on a conditional basis!
    Gleaned this from here: http://bit.ly/21WTLfs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,12 @@
-Application.ensure_all_started(:hound)
-ExUnit.start
+# Check if the phantomjs server needed for integration tests is running...
+case :gen_tcp.connect('localhost', 8910, []) do
+  {:ok, socket} ->
+    :gen_tcp.close(socket)
+    Application.ensure_all_started(:hound)
+  {:error, _reason} ->
+    ExUnit.configure(exclude: [needs_phantomjs: true])
+end
+
+ExUnit.start()
 
 Ecto.Adapters.SQL.begin_test_transaction(Gebetsgruppe.Repo)
